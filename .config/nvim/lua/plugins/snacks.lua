596258c8fd0eba58831fd2fedab8c7286fc91da4
bishalr0y <bishalroy895@gmail.com>
2025-03-31 20:41:27 +0530

    trying snacks.nvim

---
+++ .config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,13 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  opts = {
+    bigfile = { enabled = true },
+    dashboard = {
+      example = "compact_files",
+    },
+    explorer = { enabled = true },
+    notifier = { enabled = true },
+  },
+}
