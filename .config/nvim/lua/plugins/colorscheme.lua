b42ce2723fee8ff3c2a79f3b3a5d31b3167e1a75
bishalr0y <bishalroy895@gmail.com>
2025-04-08 21:24:08 +0530

    updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,6 +7,7 @@ return {
     config = function()
       require("tokyonight").setup({
         style = "night",
+        transparent = false,
       })
       vim.cmd.colorscheme("tokyonight")
     end,
