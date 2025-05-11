f04727acb7de65b5a63730dd7da956e99f3d4835
bishalr0y <bishalroy895@gmail.com>
2025-05-11 22:27:53 +0530

    modified keymaps

--- .config/kickstart.nvim/lua/custom/keymaps.lua
+++ .config/kickstart.nvim/lua/custom/keymaps.lua
@@ -3,10 +3,10 @@ vim.keymap.set("n", "<leader>-", ":split<CR>", { desc = "Horizontal Split" })
 vim.keymap.set("n", "<leader>|", ":vsplit<CR>", { desc = "Vertical Split" })
 
 -- Navigate between splits
-vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Move to Left Window" })
-vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Move to Lower Window" })
-vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Move to Upper Window" })
-vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Move to Right Window" })
+vim.keymap.set("n", "<leader>wh", "<C-w>h", { desc = "Move to Left Window" })
+vim.keymap.set("n", "<leader>wj", "<C-w>j", { desc = "Move to Lower Window" })
+vim.keymap.set("n", "<leader>wk", "<C-w>k", { desc = "Move to Upper Window" })
+vim.keymap.set("n", "<leader>wl", "<C-w>l", { desc = "Move to Right Window" })
 
 -- Delete (close) a split
-vim.keymap.set("n", "<leader>wc", "<C-w>c", { desc = "Close Window" })
+vim.keymap.set("n", "<leader>wd", "<C-w>c", { desc = "Close Window" })
