6c7b80d149b26fd3f8f36a69d5efe08cb9f4a78e
bishalr0y <bishalroy895@gmail.com>
2025-05-13 13:46:21 +0530

    update

--- .config/kickstart.nvim/init.lua
+++ .config/kickstart.nvim/init.lua
@@ -633,7 +633,7 @@ require("lazy").setup({
       },
 
       -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false }, ghost_text = { enabled = true } },
+      completion = { documentation = { auto_show = true }, ghost_text = { enabled = true } },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`
