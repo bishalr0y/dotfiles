a97353849c71ae49b2f12c7bcc935490e7369e42
bishalr0y <bishalroy895@gmail.com>
2025-05-21 23:34:36 +0530

    add new keymaps for moving line

--- .config/kickstart.nvim/lua/custom/keymaps.lua
+++ .config/kickstart.nvim/lua/custom/keymaps.lua
@@ -28,3 +28,7 @@ vim.keymap.set("n", "<leader>cd", vim.diagnostic.open_float, { desc = "Show line
 
 -- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
 vim.keymap.set("t", "<Esc><Esc>", "<C-\\><C-n>", { desc = "Exit terminal mode" })
+
+-- Move line up and down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
