9779f48900dbe9634668a544ea0ad394b41fa8d3
bishalr0y <bishalroy895@gmail.com>
2025-05-29 08:31:38 +0530

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -8,11 +8,11 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
   if vim.v.shell_error ~= 0 then
     error("Error cloning lazy.nvim:\n" .. out)
   end
-end ---@diagnostic disable-next-line: undefined-field
+end
+---@diagnostic disable-next-line: undefined-field
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   "tpope/vim-sleuth", -- Detect tabstop and shiftwidth automatically
 
   -- require 'kickstart.plugins.debug',
@@ -23,24 +23,4 @@ require("lazy").setup({
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   { import = "custom.plugins" },
-}, {
-  ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = "⌘",
-      config = "🛠",
-      event = "📅",
-      ft = "📂",
-      init = "⚙",
-      keys = "🗝",
-      plugin = "🔌",
-      runtime = "💻",
-      require = "🌙",
-      source = "📄",
-      start = "🚀",
-      task = "📌",
-      lazy = "💤 ",
-    },
-  },
 })
--- .config/nvim/lua/custom/plugins/blink.lua
+++ .config/nvim/lua/custom/plugins/blink.lua
@@ -8,7 +8,7 @@ return {
     appearance = { nerd_font_variant = "mono" },
     completion = {
       documentation = { auto_show = true },
-      ghost_text = { enabled = true },
+      ghost_text = { enabled = false },
     },
     signature = {
       enabled = true,
