01ad85246f4fe6e6c4605acaf1967b71b4244216
bishalr0y <bishalroy895@gmail.com>
2025-06-16 20:41:58 +0530

    experimenting..

--- .config/nvim/lua/custom/plugins/colorscheme.lua
+++ .config/nvim/lua/custom/plugins/colorscheme.lua
@@ -1,5 +1,23 @@
 return {
   {
+    "sainnhe/gruvbox-material",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.gruvbox_material_enable_italic = 0
+      vim.g.gruvbox_material_disable_italic_comment = 1
+      vim.g.gruvbox_material_background = "hard"
+      vim.g.gruvbox_material_ui_contrast = "high"
+      vim.g.gruvbox_material_float_style = "dim"
+      vim.g.gruvbox_material_better_performance = 1
+      vim.g.gruvbox_material_foreground = "material"
+      vim.g.gruvbox_material_enable_bold = 1
+      vim.g.gruvbox_material_transparent_background = 0 --values (0,1,2)
+
+      vim.cmd.colorscheme("gruvbox-material")
+    end,
+  },
+  {
     "folke/tokyonight.nvim",
     priority = 1000,
     config = function()
@@ -12,7 +30,7 @@ return {
           variables = {},
         },
       })
-      vim.cmd.colorscheme("tokyonight-night")
+      -- vim.cmd.colorscheme("tokyonight-night")
       -- Make the Cursor line transparent
       vim.api.nvim_set_hl(0, "CursorLine", { bg = "none" })
     end,
