6420099e392854e4b435182d580cbb2cee83eb49
bishalr0y <bishalroy895@gmail.com>
2025-07-07 11:34:05 +0530

    add fish config

---
+++ .config/fish/conf.d/aliases.fish
@@ -0,0 +1,3 @@
+alias c 'clear'
+alias grep 'grep --color=auto --exclude-dir={.bzr,CVS,.git,.hg,.svn,.idea,.tox}'
+alias ks 'tmux kill-server'
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,3 +1,12 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
 end
+
+# Disable greeting
+set fish_greeting 
+
+# Set Editor to neovim
+set -gx EDITOR 'nvim'
+
+# Set neovim as the program to open manpages
+set -gx MANPAGER 'nvim +Man!'
