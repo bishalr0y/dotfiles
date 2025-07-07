207671e7ba83f0446e305d8c2b70bdb19b5fdd91
bishalr0y <bishalroy895@gmail.com>
2025-07-07 22:33:24 +0530

    update todo-comments.nvim config

--- .config/nvim/lua/custom/plugins/todo-comments.lua
+++ .config/nvim/lua/custom/plugins/todo-comments.lua
@@ -1,7 +1,11 @@
--- Highlight todo, notes, etc in comments
 return {
   "folke/todo-comments.nvim",
   event = "VimEnter",
   dependencies = { "nvim-lua/plenary.nvim" },
-  opts = { signs = false },
+  opts = {
+    signs = true,
+    highlight = {
+      multiline = false,
+    },
+  },
 }
