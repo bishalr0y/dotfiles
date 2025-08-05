935a92187dae4427fa74bc4535e716c58349cd96
bishalr0y <bishalroy895@gmail.com>
2025-08-05 21:29:03 +0530

    update undotree config

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -7,12 +7,27 @@ return {
     config = function()
       require("catppuccin").setup({
         flavour = "macchiato",
-        transparent_background = false,
-        no_bold = false,
-        no_italic = false,
-        no_underline = false,
+        -- transparent_background = false,
+        -- no_bold = false,
+        -- no_italic = false,
+        -- no_underline = false,
       })
-      vim.cmd.colorscheme("catppuccin")
+      local palette = require("catppuccin.palettes").get_palette("macchiato")
+      vim.cmd.colorscheme("catppuccin-macchiato")
+
+      -- Telescope highlights to match editor background
+      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = palette.mauve, bg = palette.base })
     end,
   },
 }
--- .config/nvim/lua/plugins/undotree.lua
+++ .config/nvim/lua/plugins/undotree.lua
@@ -1,8 +1,15 @@
 return {
   "jiaoshijie/undotree",
   dependencies = "nvim-lua/plenary.nvim",
-  config = true,
+  name = "undotree",
   keys = {
     { "<leader>u", "<cmd>lua require('undotree').toggle()<cr>", desc = "Toggle undotree" },
   },
+  config = function()
+    require("undotree").setup({
+      window = {
+        winblend = 10,
+      },
+    })
+  end,
 }
