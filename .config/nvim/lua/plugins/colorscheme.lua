71f5141c3de996de59c132530896e605d8fb98aa
bishalr0y <bishalroy895@gmail.com>
2026-03-29 21:07:30 +0530

    updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -50,7 +50,7 @@ return {
       vim.g.gruvbox_material_foreground = "material"
       vim.g.gruvbox_material_enable_bold = 0
       vim.g.gruvbox_material_transparent_background = 0 --values (0,1,2)
-      vim.g.gruvbox_material_float_style = "dim"
+      vim.g.gruvbox_material_float_style = "blend"
 
       vim.cmd.colorscheme("gruvbox-material")
     end,
