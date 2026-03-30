eff8b62ad720da3d9a179553398c7254ce1a3fff
bishalr0y <bishalroy895@gmail.com>
2026-03-30 11:58:41 +0530

    updates

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -16,21 +16,21 @@ return {
         },
       })
       local palette = require("catppuccin.palettes").get_palette("macchiato")
-      -- vim.cmd.colorscheme("catppuccin-macchiato")
+      vim.cmd.colorscheme("catppuccin-macchiato")
 
       -- Telescope highlights to match editor background
-      -- vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = palette.blue, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = palette.blue, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = palette.blue, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = palette.blue, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = palette.mauve, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = palette.mauve, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = palette.mauve, bg = palette.base })
-      -- vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = palette.blue, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = palette.mauve, bg = palette.base })
+      vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = palette.mauve, bg = palette.base })
 
       -- underline only, no background
       -- vim.api.nvim_set_hl(0, "LspReferenceText", { underline = true, bg = "NONE" })
@@ -38,21 +38,21 @@ return {
       -- vim.api.nvim_set_hl(0, "LspReferenceWrite", { underline = true, bg = "NONE" })
     end,
   },
-  {
-    "sainnhe/gruvbox-material",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_enable_italic = true
-      vim.g.gruvbox_material_background = "medium"
-      vim.g.gruvbox_material_visual = "red background"
-      vim.g.gruvbox_material_better_performance = 1
-      vim.g.gruvbox_material_foreground = "material"
-      vim.g.gruvbox_material_enable_bold = 0
-      vim.g.gruvbox_material_transparent_background = 0 --values (0,1,2)
-      vim.g.gruvbox_material_float_style = "blend"
-
-      vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
+  -- {
+  --   "sainnhe/gruvbox-material",
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     vim.g.gruvbox_material_enable_italic = true
+  --     vim.g.gruvbox_material_background = "medium"
+  --     vim.g.gruvbox_material_visual = "red background"
+  --     vim.g.gruvbox_material_better_performance = 1
+  --     vim.g.gruvbox_material_foreground = "material"
+  --     vim.g.gruvbox_material_enable_bold = 0
+  --     vim.g.gruvbox_material_transparent_background = 0 --values (0,1,2)
+  --     vim.g.gruvbox_material_float_style = "blend"
+  --
+  --     -- vim.cmd.colorscheme("gruvbox-material")
+  --   end,
+  -- },
 }
