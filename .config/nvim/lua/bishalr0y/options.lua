ab8e149da633b8eeafa90c271b26bd4da693736a
bishalr0y <bishalroy895@gmail.com>
2026-07-19 20:57:38 +0530

    updates

--- .config/nvim/lua/bishalr0y/options.lua
+++ .config/nvim/lua/bishalr0y/options.lua
@@ -34,9 +34,7 @@ vim.opt.breakindent = true
 -- Set the tab size to 2 spaces
 vim.o.tabstop = 2
 vim.o.softtabstop = 2
-vim.o.shiftwidth = 2
 vim.o.expandtab = true
-vim.o.smartindent = true
 
 -- Save undo history
 vim.opt.undofile = true
