191df71b10bd4ce97109708b68bb32b78eae4550
bishalr0y <bishalroy895@gmail.com>
2026-08-29 11:59:41 +0530

    updates

--- home/.config/nvim/lua/plugins/colorscheme.lua
+++ home/.config/nvim/lua/plugins/colorscheme.lua
@@ -3,23 +3,6 @@ return {
     "catppuccin/nvim",
     name = "catppuccin",
     priority = 1000,
-    config = function()
-      local palette = require("catppuccin.palettes").get_palette("mocha")
-
-      -- Telescope highlights to match editor background
-      vim.api.nvim_set_hl(0, "TelescopeNormal", { bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = palette.blue, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePromptNormal", { bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePromptBorder", { fg = palette.blue, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopeResultsNormal", { bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopeResultsBorder", { fg = palette.blue, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePreviewNormal", { bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePreviewBorder", { fg = palette.blue, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopeTitle", { fg = palette.mauve, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePromptTitle", { fg = palette.mauve, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopeResultsTitle", { fg = palette.mauve, bg = palette.base })
-      vim.api.nvim_set_hl(0, "TelescopePreviewTitle", { fg = palette.mauve, bg = palette.base })
-    end,
   },
   {
     "LazyVim/LazyVim",
